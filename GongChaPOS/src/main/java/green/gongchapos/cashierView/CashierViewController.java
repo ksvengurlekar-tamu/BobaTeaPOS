924544596e71078e2739a9b81c25f6f3462973ec
Jedidiah Israel <jedi@Jedidiahs-MacBook-Pro-3.local>
2023-10-10 15:38:52 -0500

    Worked on pulling data from SQL table to load into GUI

--- GongChaPOS/src/main/java/green/gongchapos/cashierView/CashierViewController.java
+++ GongChaPOS/src/main/java/green/gongchapos/cashierView/CashierViewController.java
@@ -1,4 +1,40 @@
 package green.gongchapos.cashierView;
 
+import green.gongchapos.dbSetup;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+import java.sql.*;
+
 public class CashierViewController {
+
+    @FXML
+    String seriesName;
+
+
+    @FXML
+    private void selectSeries(ActionEvent actionEvent) {
+        Connection conn = null;
+        String teamName = "00g";
+        String dbName = "csce315331_" + teamName + "_db";
+        String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+
+        try {
+            conn = DriverManager.getConnection(dbConnectionString, dbSetup.user, dbSetup.pswd);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+        try {
+            String selectSeriesSQL = "SELECT menuItemName FROM menuItems WHERE menuItemCategory = " + seriesName;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
 }
